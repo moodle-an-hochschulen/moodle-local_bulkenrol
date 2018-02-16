ae8c1bce10f66f32a1d48d20062a2e8e729600aa
Alexander Bias <alexander.bias@uni-ulm.de>
2018-02-16 11:33:31 +0100

    Cleanup index.php

--- index.php
+++ index.php
@@ -15,38 +15,35 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Seite Einschreiben von Usern anhand einer Emailliste
+ * Local plugin "bulkenrol" - Enrolment page
  *
- * @package    local
- * @subpackage bulkenrol
- * @copyright  2017 Soon-Systems GmbH {@link https://soon-systems.de}
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package   local_bulkenrol
+ * @copyright 2017 Soon Systems GmbH on behalf of Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once('../../config.php');
+
 require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->dirroot.'/local/bulkenrol/locallib.php');
 require_once($CFG->dirroot.'/local/bulkenrol/bulkenrol_form.php');
 require_once($CFG->dirroot.'/local/bulkenrol/confirm_form.php');
 
 
-###################################################################
-
-$id     = optional_param('id', 0, PARAM_INT); // This are required.
-$local_bulkenrol_key = optional_param('key', 0, PARAM_RAW);
+$id = optional_param('id', 0, PARAM_INT); // This are required.
+$localbulkenrolkey = optional_param('key', 0, PARAM_RAW);
 
 $context = context_system::instance();
 
-if(!empty($id)){
+if (!empty($id)) {
     $course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
     $context = context_course::instance($course->id, MUST_EXIST);
-    
+
     $PAGE->set_context($context);
     $PAGE->set_url('/local/bulkenrol/index.php', array('id' => $id));
     $PAGE->set_title("$course->shortname: ".get_string('pluginname', 'local_bulkenrol'));
     $PAGE->set_heading($course->fullname);
-    
-    
+
     require_login($course);
 }
 
@@ -57,32 +54,29 @@ if (!has_capability('local/bulkenrol:enrolusers', $context)) {
 $PAGE->set_pagelayout('incourse');
 
 
-if(empty($local_bulkenrol_key)){
+if (empty($localbulkenrolkey)) {
     $form = new local_bulkenrol_form(null, array('courseid' => $id));
     if ($formdata = $form->get_data()) {
         $emails = $formdata->usermails;
         $courseid = $formdata->id;
-        
-        $checked_mails = local_bulkenrol_check_user_mails($emails);
-        
-        // Create local_bulkenrol array in Session
-        if(!isset($SESSION->local_bulkenrol)){
+
+        $checkedmails = local_bulkenrol_check_user_mails($emails);
+
+        // Create local_bulkenrol array in Session.
+        if (!isset($SESSION->local_bulkenrol)) {
             $SESSION->local_bulkenrol = array();
         }
-        // Save data in Session
-        $local_bulkenrol_key = $courseid.'_'.time();
-        $SESSION->local_bulkenrol[$local_bulkenrol_key] = $checked_mails;
-        
-    } 
-    else if($form->is_cancelled()){
-        if(!empty($id)){
+        // Save data in Session.
+        $localbulkenrolkey = $courseid.'_'.time();
+        $SESSION->local_bulkenrol[$localbulkenrolkey] = $checkedmails;
+
+    } else if ($form->is_cancelled()) {
+        if (!empty($id)) {
             redirect($CFG->wwwroot .'/course/view.php?id='.$id, '', 0);
-        }
-        else{
+        } else {
             redirect($CFG->wwwroot, '', 0);
         }
-    }
-    else {
+    } else {
         echo $OUTPUT->header();
         echo $OUTPUT->heading(get_string('pluginname', 'local_bulkenrol'));
         echo $form->display();
@@ -90,45 +84,39 @@ if(empty($local_bulkenrol_key)){
     }
 }
 
-if ($local_bulkenrol_key) {
-    $form2 = new local_bulkenrol_confirm_form(null, array('local_bulkenrol_key' => $local_bulkenrol_key, 'courseid' => $id));
-    
+if ($localbulkenrolkey) {
+    $form2 = new local_bulkenrol_confirm_form(null, array('local_bulkenrol_key' => $localbulkenrolkey, 'courseid' => $id));
+
     if ($formdata = $form2->get_data()) {
         global $SESSION;
-    
-        if(!empty($local_bulkenrol_key) && !empty($SESSION->local_bulkenrol) && array_key_exists($local_bulkenrol_key, $SESSION->local_bulkenrol)){
+
+        if (!empty($localbulkenrolkey) && !empty($SESSION->local_bulkenrol) &&
+                array_key_exists($localbulkenrolkey, $SESSION->local_bulkenrol)) {
             set_time_limit(600);
-    
-//             echo $OUTPUT->header();
-//             echo $OUTPUT->heading(get_string('pluginname', 'local_bulkenrol'));
-            
-            local_bulkenrol_users($local_bulkenrol_key);
-    
+
+            local_bulkenrol_users($localbulkenrolkey);
+
             redirect($CFG->wwwroot .'/local/bulkenrol/index.php?id='.$id, '', 0);
-//             echo $OUTPUT->footer();
-        }
-        else{
+        } else {
             redirect($CFG->wwwroot .'/local/bulkenrol/index.php?id='.$id, '', 0);
         }
-    }
-    else if($form2->is_cancelled()){
+    } else if ($form2->is_cancelled()) {
         redirect($CFG->wwwroot .'/local/bulkenrol/index.php?id='.$id, '', 0);
-    }
-    else {
-        
+    } else {
         $PAGE->set_url('/local/bulkenrol/index.php', array('id' => $id));
-        
+
         echo $OUTPUT->header();
         echo $OUTPUT->heading(get_string('pluginname', 'local_bulkenrol'));
-        if(!empty($local_bulkenrol_key) && !empty($SESSION->local_bulkenrol) && array_key_exists($local_bulkenrol_key, $SESSION->local_bulkenrol)){
-            $local_bulkenrol_data = $SESSION->local_bulkenrol[$local_bulkenrol_key];
-    
-            if(!empty($local_bulkenrol_data)){
-                local_bulkenrol_display_table($local_bulkenrol_data, LOCALBULKENROL_HINT);
-                local_bulkenrol_display_table($local_bulkenrol_data, LOCALBULKENROL_ENROLUSERS);
+        if (!empty($localbulkenrolkey) && !empty($SESSION->local_bulkenrol) &&
+                array_key_exists($localbulkenrolkey, $SESSION->local_bulkenrol)) {
+            $localbulkenroldata = $SESSION->local_bulkenrol[$localbulkenrolkey];
+
+            if (!empty($localbulkenroldata)) {
+                local_bulkenrol_display_table($localbulkenroldata, LOCALBULKENROL_HINT);
+                local_bulkenrol_display_table($localbulkenroldata, LOCALBULKENROL_ENROLUSERS);
             }
         }
-    
+
         echo $form2->display();
         echo $OUTPUT->footer();
     }
