af0626492a2025efc6613845382ca79807a5468d
Alexander Bias <alexander.bias@uni-ulm.de>
2018-02-16 12:26:11 +0100

    Cleanup form classes

--- bulkenrol_form.php
+++ bulkenrol_form.php
@@ -1,40 +1,65 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local plugin "bulkenrol" - Enrolment form
+ *
+ * @package   local_bulkenrol
+ * @copyright 2017 Soon Systems GmbH on behalf of Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once $CFG->libdir.'/formslib.php';
+global $CFG;
+
+require_once($CFG->libdir.'/formslib.php');
 
 class local_bulkenrol_form extends moodleform {
-    
-    function definition () {
+
+    protected function definition() {
         global $CFG;
-        
+
         require_once($CFG->dirroot.'/local/bulkenrol/lib.php');
-        
+
         $mform = $this->_form;
-        
-        // Infotext
-        $msg = get_string('infotext','local_bulkenrol');
+
+        // Infotext.
+        $msg = get_string('infotext', 'local_bulkenrol');
         $mform->addElement('html', '<div class="local_bulkenrol infotext">'.$msg.'</div>');
-        
-        // Textarea für Emails
-        $mform->addElement('textarea', 'usermails', get_string('usermails', 'local_bulkenrol'), 'wrap="virtual" rows="10" cols="80"');
+
+        // Textarea für Emails.
+        $mform->addElement('textarea', 'usermails',
+                get_string('usermails', 'local_bulkenrol'), 'wrap="virtual" rows="10" cols="80"');
         $mform->addRule('usermails', null, 'required');
-        
+
         $mform->addElement('hidden', 'id');
         $mform->setType('id', PARAM_RAW);
         $mform->setDefault('id', $this->_customdata['courseid']);
-        
+
         $this->add_action_buttons(true, get_string('submit'));
     }
-    
-    function validation($data, $files) {
+
+    public function validation($data, $files) {
         $retval = array();
-        
-        if(empty($data['usermails'])){
+
+        if (empty($data['usermails'])) {
             $retval['usermails'] = get_string('usermails_empty', 'local_bulkenrol');
         }
-        
+
         return $retval;
     }
 }
--- confirm_form.php
+++ confirm_form.php
@@ -1,25 +1,49 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local plugin "bulkenrol" - Confirmation form
+ *
+ * @package   local_bulkenrol
+ * @copyright 2017 Soon Systems GmbH on behalf of Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once $CFG->libdir.'/formslib.php';
+global $CFG;
+
+require_once($CFG->libdir.'/formslib.php');
 
 class local_bulkenrol_confirm_form extends moodleform {
-    
-    function definition () {
-        $local_bulkenrol_key = $this->_customdata['local_bulkenrol_key'];
+
+    protected function definition() {
+        $localbulkenrolkey = $this->_customdata['local_bulkenrol_key'];
         $courseid = $this->_customdata['courseid'];
-        
+
         $mform = $this->_form;
-        
+
         $mform->addElement('hidden', 'key');
         $mform->setType('key', PARAM_RAW);
-        $mform->setDefault('key', $local_bulkenrol_key);
-        
+        $mform->setDefault('key', $localbulkenrolkey);
+
         $mform->addElement('hidden', 'id');
         $mform->setType('id', PARAM_INT);
         $mform->setDefault('id', $courseid);
-        
+
         $this->add_action_buttons(true, get_string('enrol_users', 'local_bulkenrol'));
     }
 }
