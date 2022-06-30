1e3fb0e34564a8b00fc8d65826b34aefd959c891
Alexander Bias <bias@alexanderbias.de>
2022-06-30 21:30:06 +0200

    Make codechecker happy again

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,8 +24,6 @@
 
 namespace local_bulkenrol\privacy;
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Privacy Subsystem implementing null_provider.
  *
--- locallib.php
+++ locallib.php
@@ -22,8 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 define('LOCALBULKENROL_HINT', 'hint');
 define('LOCALBULKENROL_ENROLUSERS', 'enrolusers');
 define('LOCALBULKENROL_GROUPINFOS', 'groupinfos');
